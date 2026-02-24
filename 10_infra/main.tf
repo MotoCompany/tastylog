b09fc600223c1999ac8f664f1879649f85e9d2e1
daiki1994 <dddaiki0111@outlook.com>
2026-02-24 17:27:12 +0900

    S3バケット名を追加

--- 10_infra/main.tf
+++ 10_infra/main.tf
@@ -10,10 +10,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket  = "{YOUR-S3-BUCKET-NAME}"
-    key     = "{YOUR-S3-BUCKET-KEY}"
+    bucket  = "testmoto0224"
+    key     = "tastylog-dev.tfstate"
     region  = "ap-northeast-1"
-    profile = "terraform"
+    profile = "terraform1"
   }
 }
 
